16b414774de3befdbba01b2c353d9abf63d54e48
mdraju <md raju>
2021-04-08 12:11:27 +0600

    done

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const plusButton = document.getElementById("plusBtn");
 plusButton.addEventListener("click", function() {
-    setInputValue(1);
+        productPrice(1);
 
-
-})
-
+    })
+    // =================================MobilePriceWork==============================================//
 const minusButton = document.getElementById("minusBtn");
 minusButton.addEventListener("click", function() {
-    // setInputValue(-1);
+
+    productPrice(-1);
+
 })
 
-function setInputValue(add) {
+function productPrice(addProduct) {
     const inputValue = document.getElementById("InputValue").value;
-    const inputNumber = parseFloat(inputValue);
-    const addValue = inputNumber + add;
+    const inputNumber = parseInt(inputValue);
+
+    const addValue = inputNumber + addProduct;
     document.getElementById("InputValue").value = addValue;
 
     const mobilePrice = document.getElementById("mobilePrice").innerText;
     const mobilePriceNumber = parseFloat(mobilePrice);
-    const totalPrice = mobilePriceNumber * addValue;
+    const totalPrice = addValue * 1219;
     document.getElementById("mobilePrice").innerText = totalPrice;
+
 }
+
+// ===============================================CasePriceWork=========================================//
