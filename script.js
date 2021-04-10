01d9197ce345bc9682617246a2f52da7839e47ac
mdraju <md raju>
2021-04-10 11:43:29 +0600

    done

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function productPrice(PhoneIncrease) {
     document.getElementById("InputValue").value = newInput;
     const totalPrice = newInput * 1219;
     document.getElementById("mobilePrice").innerText = totalPrice;
+    subtotalAmount();
 
 };
 // // ===============================================CasePriceWork=========================================//
@@ -32,4 +33,21 @@ function caseInputPrice(addInputNumber) {
     const newPrice = newCase * 59;
 
     document.getElementById("casePrice").innerText = newPrice;
+    subtotalAmount();
 };
+
+
+function subtotalAmount() {
+    const phoneInput = getInputValue("InputValue")
+    const caseInput = getInputValue("case-input");
+
+    const subtotal = phoneInput * 1219 + caseInput * 59;
+    document.getElementById("subtotalValue").innerText = '$' + subtotal;
+
+}
+
+function getInputValue(product) {
+    const productValue = document.getElementById(product).value;
+    const productNumber = parseInt(productValue);
+    return productNumber;
+}
